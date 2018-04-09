e6f6dec0871aa1520f5441efa3ba7aa124182219
Olof Düsterdieck <olof.dusterdieck@gmail.com>
2018-04-09 15:14:05 +0200

    First commit, lab1

---
+++ lab1/Solution1.hs
@@ -0,0 +1,102 @@
+module Solution1 where
+
+import Control.Parallel
+import Control.Parallel.Strategies
+import Control.Monad.Par
+import Given
+
+-- a) Using par & pseq
+
+-- Add granularity
+myParMap :: (a -> b) -> [a] -> [b]
+myParMap f [] = []
+myParMap f (l:ls) = par x (pseq y (x:y))
+    where 
+        x = f l
+        y = myParMap f ls
+
+
+-- b) Eval Monad
+evalJackknife :: ([a] -> b) -> [a] -> [b] 
+evalJackknife f l = runEval (parMapEval f $ resamples 500 l)
+
+parMapEval :: (a -> b) -> [a] -> Eval [b]
+parMapEval _ [] = return []
+parMapEval f (x:xs) = do
+    y  <- rpar (f x)
+    ys <- parMapEval f xs
+    return (y:ys)
+
+-- c) Strategies
+stratJackknife :: ([a] -> b) -> [a] -> [b]
+stratJackknife f xs = jackknife f xs `using` jackstrat rseq
+
+evalstrat :: Strategy a -> Strategy [a]
+evalstrat strat [] = return []
+evalstrat strat (x:xs) = do
+    y <- strat x
+    ys <- evalstrat strat xs
+    return (y:ys)
+
+jackstrat :: Strategy a -> Strategy [a]
+jackstrat strat = evalstrat (rparWith strat)
+
+
+
+-- d) Par Monad
+myspawn :: NFData a => Par a -> Par (IVar a)
+myspawn p = do
+    i <- new 
+    fork (do x <- p; put i x)
+    return i
+
+myParMapM :: NFData b => (a -> b) -> [a] -> Par [b]
+myParMapM f xs = do
+    ibs <- mapM (myspawn . return . f) xs
+    mapM get ibs
+
+--monadJackknife :: ([a] -> b) -> [a] -> [b]
+--monadJackknife f l = runPar $ do
+--    i <- mapM (myspawn . jackshort) l
+--    mapM get i
+
+monadJackknife :: NFData b => ([a] -> b) -> [a] -> [b]
+monadJackknife f l = runPar $ myParMapM (f) (resamples 500 l)
+
+-- Assignment 2
+
+-- With Strategies
+stratMerge :: Ord a => [a] -> [a]
+stratMerge l = mergesort l --'using' (mergeStrat rseq)
+
+-- Borde nog vara något lite mer parPair liknande... 
+-- Typ att du skickar rpar till parPair ... och ger rpar rseq...?
+--mergeStrat :: Strategy a -> Strategy [a]
+--mergeStrat _ [] = []
+--mergeStrat strat (l:ls) = do
+--    x <- strat l
+--    xs <- mergeStrat strat ls
+--    return (x:xs)
+
+-- Mergesort without parallism
+mergesort :: Ord a => [a] -> [a]
+mergesort [] = []
+mergesort x | l > 1 =  merge $ mapTuple (mergesort) (splitAt (quot l 2) x)
+            | otherwise = x
+            where l = length x
+
+mapTuple :: (a -> b) -> (a,a) -> (b,b)
+mapTuple f (a1,a2) = (f a1, f a2)
+
+merge :: Ord a => ([a],[a]) -> [a]
+merge ([], y) = y
+merge (x, []) = x
+merge ((x:xs), (y:ys)) 
+  | x <= y = (x:merge (xs, (y:ys)))
+  | x >  y = (y:merge ((x:xs), ys))
+
+-- With Par Monad
+--monadMerge :: Ord a => [a] -> [a]
+--monadMerge l = runPar $ do parMerge l
+
+--parMerge :: [a] -> Par [a]
