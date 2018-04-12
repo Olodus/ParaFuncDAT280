cc1c5414e7d335d60cf784a377c76092af84f690
Benedikt Sigurleifsson <benedikt12@ru.is>
2018-04-12 09:41:22 +0200

    Making larger task in main

--- lab1/Main.hs
+++ lab1/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
 import Solution1
+import System.Random
+import Given
+--main = print (stratJackknife (sum) [1..550])
 
-main = print (parpseqJackknife (sum) [1..550])
+
+main = do
+  let (xs,ys) = splitAt 1500  (take 6000
+                               (randoms (mkStdGen 211570155)) :: [Double] )
+  -- handy (later) to give same input different parallel functions
+
+  let rs = crud xs ++ ys
+  print (stratJackknife (sum) rs)
+
+
