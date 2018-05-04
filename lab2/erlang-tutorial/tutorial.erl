3c4650d1f18530f3997d8f99e4d55bf592ae883e
Benedikt Sigurleifsson <benedikt12@ru.is>
2018-05-04 11:29:01 +0200

    sequential merge sort working

--- lab2/erlang-tutorial/tutorial.erl
+++ lab2/erlang-tutorial/tutorial.erl
@@ -1,3 +1,29 @@
+-module(tutorial).
+-export([seq_msort/1]).
 
-seq_msort([]) = []
-seq_msort(x:xs) = 
+
+merge([], Y) -> Y;
+merge(X, []) -> X;
+merge([X|Xs], [Y|Ys]) -> 
+    if 
+        X < Y -> [X|merge(Xs, [Y|Ys])];
+        true -> [Y|merge(Ys, [X|Xs])]
+    end.
+
+
+seq_msort([]) -> [];
+seq_msort(X) -> 
+    Len = length(X),
+    if 
+        Len > 1 ->
+            {L1,L2} = lists:split((round(Len/2)), X),
+            merge(seq_msort(L1), seq_msort(L2));
+        true -> 
+            X
+    end.
+
+
+
+
+
+
