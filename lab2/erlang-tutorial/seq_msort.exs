f5672a12cfcec56afc92b43a235c17640c497159
Benedikt Sigurleifsson <benedikt12@ru.is>
2018-05-04 17:11:03 +0200

    Sequential merge sort is working

--- lab2/erlang-tutorial/seq_msort.exs
+++ lab2/erlang-tutorial/seq_msort.exs
@@ -8,4 +8,15 @@ defmodule SeqMergeSort do
             [y|merge(ys, [x|xs])]
         end
     end
+
+    def seq_merge_sort([]), do: []
+    def seq_merge_sort(x) do
+        len = length(x)
+        if len > 1 do
+            {l1, l2} = Enum.split(x, round(len/2))
+            merge(seq_merge_sort(l1), seq_merge_sort(l2))
+        else
+            x
+        end
+    end 
 end
