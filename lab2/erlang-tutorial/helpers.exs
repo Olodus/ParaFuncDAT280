26d605a64bd7563de96546a46f0ec9f232f2009b
Benedikt Sigurleifsson <benedikt12@ru.is>
2018-05-05 17:34:50 +0200

    Benchmark now accepts a list of argument, this way we can accept functions that receive more than one argument like gran par msort

--- lab2/erlang-tutorial/helpers.exs
+++ lab2/erlang-tutorial/helpers.exs
@@ -5,7 +5,7 @@ defmodule Helpers do
 
     def benchmark(n, module, fun, fun_input) do
         runs = for i <- 0..n, i > 0, do: 
-            :timer.tc(module, fun, [fun_input]) 
+            :timer.tc(module, fun, fun_input) 
             |> elem(0)
         Enum.sum(runs) / length(runs)
     end
