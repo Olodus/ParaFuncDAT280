4871c3d15384f866cca0d41fd82aad1ada87f575
Benedikt Sigurleifsson <benedikt12@ru.is>
2018-05-05 15:50:57 +0200

    Finished benchmarking function

--- lab2/erlang-tutorial/helpers.exs
+++ lab2/erlang-tutorial/helpers.exs
@@ -2,4 +2,11 @@ defmodule Helpers do
     def random_list(interval, n) do
         Enum.map(1..n, fn _ -> :rand.uniform(interval) end) 
     end
+
+    def benchmark(n, module, fun, fun_input) do
+        runs = for i <- 0..n, i > 0, do: 
+            :timer.tc(module, fun, [fun_input]) 
+            |> elem(0)
+        Enum.sum(runs) / length(runs)
+    end
 end
