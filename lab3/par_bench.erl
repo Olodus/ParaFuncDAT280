fde30271671f123ce84a23fc0f37d6f3b9d9ce53
Benedikt Sigurleifsson <benedikt12@ru.is>
2018-05-08 19:16:36 +0200

    Parallel benchmark is ready

--- lab3/par_bench.erl
+++ lab3/par_bench.erl
@@ -1,4 +1,32 @@
+-module(par_bench).
+-import(sudoku, [solve/1]).
+-import(pool, [pool_start/1]).
+-compile(export_all).
 
 %% Runs the benchmark in parallel by splitting it onto a worker pool.
-par_benchmark(Puzzles) ->
+par_benchmarks(Puzzles) -> 
+    Pp = pool_start(10),
+    solver(Pp, Puzzles).
 
+solver(_, []) -> [];
+solver(Pool, [P|Puzzles]) ->
+    Pool ! {ask, self()},
+    {Name, M} = P,
+    receive
+        {ok, Wp} -> Wp ! {work, self(), fun() -> solve(M) end, []},
+                    receive {result, _, R} ->
+                        [{Name, R}] ++ helper(Pool, Puzzles)
+                    end
+        %{no_avail, _} ->  
+    end.
+
+
+
+
+
+%benchmarks(Puzzles) ->
+%    [{Name,bm(fun()->solve(M) end)} || {Name,M} <- Puzzles].
+
+benchmarks() ->
+  {ok,Puzzles} = file:consult("problems.txt"),
+  timer:tc(?MODULE,par_benchmarks,[Puzzles]).
