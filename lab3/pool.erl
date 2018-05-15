20f93fc84482f6d4ad84821a6b773ad077e64d04
Olof Düsterdieck <olof.dusterdieck@gmail.com>
2018-05-15 12:46:21 +0200

    A almost working par refine

--- lab3/pool.erl
+++ lab3/pool.erl
@@ -15,14 +15,14 @@ start_workers(X, Pp) -> [spawn_link(pool, worker, [Pp]) | start_workers(X-1, Pp)
 %% Calls itself
 pool_loop([]) ->
     receive 
-        {ask, Pid} -> Pid ! {no_avail, 0}, pool_loop([]);
+        {ask, Pid, Ref} -> Pid ! {no_avail, Ref, 0}, pool_loop([]);
         {available, Pid} -> pool_loop([Pid]);
         {new_ws, Workers} -> pool_loop(Workers)
         %{exit, Pid} ->  %% handle shutdown of workers.
     end;
 pool_loop([X | Xs]) ->
     receive 
-        {ask, Pid} -> Pid ! {ok, X}, 
+        {ask, Pid, Ref} -> Pid ! {ok, Ref, X}, 
                       pool_loop(Xs);
         {available, Pid} -> pool_loop([Pid] ++ [X|Xs]);
         {new_ws, Workers} -> pool_loop(Workers ++ [X|Xs])
@@ -34,8 +34,8 @@ pool_loop([X | Xs]) ->
 %% Runs the given work, sends it back and then makes itself available again.
 worker(Pool_pid) ->
     receive
-        {work, Pid, F, Args} ->  
-                                Pid ! {result, self(), apply(F, Args)}, 
+        {work, Pid, Ref, F, Args} ->  
+                                Pid ! {result, self(), Ref, apply(F, Args)}, 
                                 Pool_pid ! {available, self()}, 
                                 worker(Pool_pid)
     end.
